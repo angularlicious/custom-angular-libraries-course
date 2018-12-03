e1fe87578982d7661652d6d933bc32e4a7c36f9f
Matt Vaughn <matt.vaughn@buildmotion.com>
2018-12-03 00:44:46 -0500

    feat(libs) use custom lib in component

--- getting-started-with-libs/apps/logging-consumer/src/app/app.component.ts
+++ getting-started-with-libs/apps/logging-consumer/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoggingWithConfigService } from '@angularlicious/logging-with-config';
 
 @Component({
   selector: 'angularlicious-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+ 
   title = 'logging-consumer';
+
+  constructor(
+    private loggingService: LoggingWithConfigService
+  ) {
+    this.loggingService.log(`Running constructor from AppComponent`);
+  }
+
+  ngOnInit(): void {
+    this.loggingService.log(`Running ngOnInit from AppComponent`);
+  }
 }
--- getting-started-with-libs/libs/logging-with-config/src/index.ts
+++ getting-started-with-libs/libs/logging-with-config/src/index.ts
@@ -1,1 +1,4 @@
 export * from './lib/logging-with-config.module';
+
+export { LoggingConfig } from './lib/logging-config';
+export { LoggingWithConfigService } from './lib/logging-with-config.service';
